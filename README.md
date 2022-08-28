257278cf9aacb8ea9b6e9860cb1685fcba54cc8e
gustavovalle23 <gustavo.vallerp@hotmail.com>
2022-08-27 21:12:00 -0300

    :tada: initial template for django user microservice app with user model

---
+++ README.md
@@ -0,0 +1,19 @@
+### Download do projeto
+```
+git clone https://github.com/churros-py/user-microservice-python.git
+```
+
+### Acessar o diretório do projeto
+```
+cd user-microservice-python
+```
+
+### Inicialização do ambiente virtual
+```
+python -m venv venv && source venv/bin/activate
+```
+
+### Iniciar o servidor local (estará disponível na no endereço http://127.0.0.1:8000 ou localhost:8000)
+```
+python manage.py runserver
+```
---
+++ api/models.py
@@ -0,0 +1,71 @@
+from django.db import models
+from django.core.mail import send_mail
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.base_user import AbstractBaseUser
+
+from django.contrib.auth.base_user import BaseUserManager
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        """
+        Creates and saves a User with the given email and password.
+        """
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user: User = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True_.')
+
+        return self._create_user(email, password, **extra_fields)
+
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField('email address', unique=True)
+    first_name = models.CharField('first name', max_length=30, blank=True)
+    last_name = models.CharField('last name', max_length=30, blank=True)
+    date_joined = models.DateTimeField('date joined', auto_now_add=True)
+    is_active = models.BooleanField('active', default=True)
+    is_staff = models.BooleanField('active', default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
+
+    def get_full_name(self):
+        '''
+        Returns the first_name and the last_name
+        '''
+        full_name = '%s %s' % (self.first_name, self.last_name)
+        return full_name.strip()
+
+    def get_short_name(self):
+        '''
+        Returns the short name for the user.
+        '''
+        return self.first_name
+
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        '''
+        Sends an email to this User.
+        '''
+        send_mail(subject, message, from_email, [self.email], **kwargs)
