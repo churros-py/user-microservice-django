b3b5664ec5c371f10e6d7eaf4816de4b913db2b5
gustavovalle23 <gustavo.vallerp@hotmail.com>
2022-08-28 17:40:32 -0300

    :sparkles: adding jwt implementation in login case

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+
 from api import views
 
 app_name = 'api'
 urlpatterns = [
-	path('', views.Login.as_view(), name='login')
+	path('api/login/', views.Login.as_view(), name='login'),
 ]
--- setup/settings.py
+++ setup/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 	'api.apps.ApiConfig',
 	'django_extensions',
 	'rest_framework',
+	'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
@@ -129,5 +130,9 @@ AUTH_USER_MODEL = 'api.User'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+	    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+
 }
