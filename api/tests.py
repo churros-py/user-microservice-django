de392f231a1f966c2cf773b284645a55103b3473
gustavovalle23 <gustavo.vallerp@hotmail.com>
2022-08-28 01:52:19 -0300

    :sparkles: adding poetry config and initial pytest configuration

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,8 @@
-from django.test import TestCase
+import pytest
 
-# Create your tests here.
+from api.models import User
+
+@pytest.mark.django_db
+def test_my_user():
+	count_users = User.objects.all().count()
+	assert count_users == 0
